4ae3496333d3c8ecfcbb3946896611a54d671447
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-01 16:16:24 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,9 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+
+def any_phrase(phrase)
+  
